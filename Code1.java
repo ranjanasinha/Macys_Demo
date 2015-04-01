86be08b0f3814c10a2df467921476f06232b945e
ranjanasinha <rsinha@nisum.com>
2015-04-01 11:26:29 +0530

    updated

--- Code1.java
+++ Code1.java
@@ -2,10 +2,10 @@ public class Code1
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello World from Code1");
-		System.out.println("Hello World from Code1");
-		System.out.println("Hello World from Code1");
-		System.out.println("Hello World from Code1");
+		System.out.println("Hello World from Code1 once");
+		System.out.println("Hello World from Code1 twice");
+		System.out.println("Hello World from Code1 thrice");
+		System.out.println("Hello World from Code1 four times");
 	}
 }
 		
